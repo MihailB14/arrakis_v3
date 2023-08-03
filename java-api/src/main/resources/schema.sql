c3e13fd4cd11715f9d6a9d8b58d9885b429919c9
ktibrewala <krish.tibrewala@gmail.com>
2023-08-03 09:30:15 +0100

    Fixed 'date' issue: now all dates are date variables

--- java-api/src/main/resources/schema.sql
+++ java-api/src/main/resources/schema.sql
@@ -32,12 +32,12 @@ CREATE TABLE IF NOT EXISTS bond(
     bond_currency ENUM('USD','GBP') NOT NULL,
     issuer_name VARCHAR(200) NOT NULL,
     face_value INT NOT NULL,
-    bond_maturity_date VARCHAR(50) NOT NULL,
+    bond_maturity_date DATE NOT NULL,
     bond_status ENUM('active', 'inactive') NOT NULL,
     bond_type ENUM('CORP','GOVN','SOVN') NOT NULL,
     bond_book_number VARCHAR(100),
     FOREIGN KEY (bond_book_number) REFERENCES books(book_number)
-)AS SELECT "ISIN",max("CUSIP"), max("UNIT_PRICE"), max("COUPON_PERCENT"),max("BOND_CURRENCY"), max("ISSUER_NAME"), max("face_value (mn)"), max("BOND_MATURITY_DATE"), max("STATUS"), max("TYPE"), max(lower("BOOK_NAME")) FROM CSVREAD('C:\work\arrakis_v3\java-api\src\main\\resources\db-bonds-data.csv') GROUP BY "ISIN";
+)AS SELECT "ISIN",max("CUSIP"), max("UNIT_PRICE"), max("COUPON_PERCENT"),max("BOND_CURRENCY"), max("ISSUER_NAME"), max("face_value (mn)"), max(PARSEDATETIME("BOND_MATURITY_DATE", 'dd/MM/yyyy')), max("STATUS"), max("TYPE"), max(lower("BOOK_NAME")) FROM CSVREAD('C:\work\arrakis_v3\java-api\src\main\\resources\db-bonds-data.csv') GROUP BY "ISIN";
 
 CREATE TABLE IF NOT EXISTS bond_holder(
     bond_holder_name VARCHAR(100) PRIMARY KEY
@@ -48,16 +48,16 @@ CREATE TABLE IF NOT EXISTS trade (
     trade_type ENUM('sell', 'buy') NOT NULL,
     trade_currency ENUM('USD','GBP') NOT NULL,
     quantity INT NOT NULL,
-    trade_settlement_date VARCHAR(50) NOT NULL,
+    trade_settlement_date DATE NOT NULL,
     trade_status ENUM('open' , 'closed') NOT NULL,
-    trade_date VARCHAR(50) NOT NULL,
+    trade_date DATE NOT NULL,
     bond_holder VARCHAR(50) NOT NULL,
     bond_reference VARCHAR(12),
     book_reference VARCHAR(100),
     FOREIGN KEY (bond_holder) REFERENCES bond_holder(bond_holder_name),
     FOREIGN KEY (bond_reference) REFERENCES bond(isin),
     FOREIGN KEY (book_reference) REFERENCES books(book_number)
-) AS SELECT "TRADE_TYPE", "TRADE_CURRENCY","QUANTITY", "TRADE_SETTLEMENT_DATE", "TRADE_STATUS", "TRADE_DATE", "BOND_HOLDER", "ISIN", lower("BOOK_NAME") FROM CSVREAD('C:\work\arrakis_v3\java-api\src\main\\resources\db-bonds-data.csv') ;
+) AS SELECT "TRADE_TYPE", "TRADE_CURRENCY","QUANTITY", PARSEDATETIME("TRADE_SETTLEMENT_DATE",'dd/MM/yyyy'), "TRADE_STATUS", PARSEDATETIME("TRADE_DATE",'dd/MM/yyyy'), "BOND_HOLDER", "ISIN", lower("BOOK_NAME") FROM CSVREAD('C:\work\arrakis_v3\java-api\src\main\\resources\db-bonds-data.csv') ;
 
 ALTER TABLE trade ADD trade_id INT AUTO_INCREMENT;
 ALTER TABLE trade ADD PRIMARY KEY (trade_id);
