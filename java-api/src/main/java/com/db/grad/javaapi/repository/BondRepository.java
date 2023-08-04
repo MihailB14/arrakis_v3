e06081331459f78f57071553148b8a292181f54b
Mihail <mihaibarbulescu2010@yahoo.com>
2023-08-04 16:11:58 +0100

    new query for getAllBonds

--- java-api/src/main/java/com/db/grad/javaapi/repository/BondRepository.java
+++ java-api/src/main/java/com/db/grad/javaapi/repository/BondRepository.java
@@ -13,3 +13,10 @@ public interface BondRepository extends JpaRepository<Bond, String> {
     @Query(nativeQuery = true, value = "select * from trade inner join bond on trade.BOND_REFERENCE=bond.ISIN where trade.BOOK_REFERENCE IN (select BOOK from book_users join users on book_users.USER_EMAIL=users.EMAIL where users.EMAIL=:email)")
     List<Bond> findAllForSpecificUser(User email);
 }
+
+
+//drop table if exists userTrades;
+//
+//        create table userTrades as select trade.BOND_REFERENCE from trade where trade.BOOK_REFERENCE in (select BOOK from book_users join users on book_users.USER_EMAIL=users.EMAIL where users.EMAIL = 'user2@gmail.com');
+//
+//        select * from bond where bond.ISIN in (select * from userTrades);
