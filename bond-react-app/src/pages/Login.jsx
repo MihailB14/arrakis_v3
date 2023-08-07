118855c9be844f6dc79547f51bec802f6cbfafcd
KiitanD <kndside@gmail.com>
2023-08-07 16:26:07 +0100

    Login updates

--- bond-react-app/src/pages/Login.jsx
+++ bond-react-app/src/pages/Login.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
-
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
+        
 const Login = () => {
+    const firebaseConfig = {
+        apiKey: "AIzaSyD1ycjD9NhR6Jw6XTDYV-LNuZ6YNDIczL0",
+        authDomain: "bondsapp-3748f.firebaseapp.com",
+        projectId: "bondsapp-3748f",
+        storageBucket: "bondsapp-3748f.appspot.com",
+        messagingSenderId: "579830957232",
+        appId: "1:579830957232:web:6a67e55d7eb6f945e36775"
+    };
+
+    // Initialize Firebase
+    //                  const app = initializeApp(firebaseConfig);
+    firebase.initializeApp(firebaseConfig);
+    function tryLogin() {
+    firebase
+        .auth()
+        .signInWithEmailAndPassword(document.getElementById("email").value, document.getElementById("password").value)
+        .catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+        });
+        }
   return (
-    <div>Login</div>
+    <div>
+    <label>Email</label>
+    <input id="email" type="text" />
+    <label>Password</label>
+    <input id="password" type="password" />
+    <button onclick={tryLogin}>Login</button>
+    </div>
+    
+
+
   )
 }
 
